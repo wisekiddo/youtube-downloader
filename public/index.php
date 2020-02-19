74813dfa96bbcd7a39c2aefd756a5e071fb3af93
mojave <ronald@wisekiddo.com>
2020-02-19 19:31:52 +0800

    sdfsdf

--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Download</title>
-</head>
-<body>
-
-
 <?php
 
 require('../vendor/autoload.php');
@@ -27,9 +18,5 @@ echo ' oead' . $links;
 echo json_encode($links, JSON_PRETTY_PRINT);
 
 
-?>
-
-</body>
-</html>
 
 
