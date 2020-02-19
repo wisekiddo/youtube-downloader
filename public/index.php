4a645b839f85011110ce6f15edbbbb9728033f30
mojave <ronald@wisekiddo.com>
2020-02-19 20:09:48 +0800

    restore

--- public/index.php
+++ public/index.php
@@ -2,80 +2,7 @@
 require('../vendor/autoload.php');
 header('Content-Type: application/json');
 
-
-$videoFormatHold = $youtubeURL = $videoTitle = $videoQuality = $videoFormat = $videoFileName = $downloadURL = $status = $statusMsg = '';
-$isVideo = 0;
-
-
-// Load and initialize downloader class
-$handler = new YouTubeDownload();
-
-// Youtube video url
-$youtubeURL = htmlspecialchars($_GET["url"]);
-
-echo "url---->" . $youtubeURL;
-
-// Check whether the url is valid
-if (!empty($youtubeURL)) {
-    // Get the downloader object
-    $downloader = $handler->getDownloader($youtubeURL);
-
-    // Set the url
-    $downloader->setUrl($youtubeURL);
-
-    echo $downloader->hasVideo();
-    // Validate the youtube video url
-    if ($downloader->hasVideo()) {
-        // Get the video download link info
-
-
-        $videoDownloadLink = $downloader->getVideoDownloadLink();
-        echo count($videoDownloadLink);
-
-        for ($x = 0; $x < count($videoDownloadLink); $x++) {
-
-            $videoFormatHold = $videoDownloadLink[$x]['format'];
-
-            if (strcmp($videoFormatHold, 'mp4') != 0) {
-                break;
-            }
-            $videoTitle = $videoDownloadLink[$x]['title'];
-            $videoQuality = $videoDownloadLink[$x]['qualityLabel'];
-            $downloadURL = $videoDownloadLink[$x]['url'];
-            $videoFormat = $videoDownloadLink[$x]['format'];
-
-        }
-
-        $result = '{"title":"' . $videoTitle . '", "format":"' . $videoFormat . '", "url":"' . $downloadURL . '"}';
-
-
-        echo json_encode($result, JSON_PRETTY_PRINT);
-
-        //$videoTitle = $videoDownloadLink[count($videoDownloadLink)-1]['title'];
-        //$videoQuality = $videoDownloadLink[count($videoDownloadLink)-1]['qualityLabel'];
-        //$videoFormat = $videoDownloadLink[count($videoDownloadLink)-1]['format'];
-        //$videoFileName = strtolower(str_replace(' ', '_', $videoTitle)).'.'.$videoFormat;
-        //$downloadURL = $videoDownloadLink[count($videoDownloadLink)-1]['url'];
-
-        $isVideo = 1;
-
-    } else {
-        $statusMsg = "Video is not found, please check YouTube URL and submit again.";
-        $status = 'error';
-        echo 'Hello ' . htmlspecialchars($_GET["url"]) . '!';
-
-    }
-} else {
-    $statusMsg = "Please enter valid YouTube URL.";
-    $status = 'error';
-    echo 'Hello ' . htmlspecialchars($_GET["url"]) . '!';
-}
-
-
-
-
-
-/*use YouTube\YouTubeDownloader;
+use YouTube\YouTubeDownloader;
 
 $youtubeURL = htmlspecialchars($_GET["url"]);
 echo $youtubeURL;
@@ -86,7 +13,7 @@ $links = $youtube->getDownloadLinks($youtubeURL);
 //echo ' oead' . $links;
 
 
-echo json_encode($links, JSON_PRETTY_PRINT);*/
+echo json_encode($links, JSON_PRETTY_PRINT);
 
 
 
