833812d4e6481edafabd7ca6a64e5807b937c78b
mojave <ronald@wisekiddo.com>
2020-02-19 18:46:29 +0800

    sample

--- public/index.php
+++ public/index.php
@@ -1,57 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>youtube-downloader</title>
-
-</head>
-<body>
-
-<!--<video width="800" height="600" controls>
-    <source src="" type="video/mp4"/>
-    <em>Sorry, your browser doesn't support HTML5 video.</em>
-</video>-->
+<?php
 
 
-<script>
+use YouTube\YouTubeDownloader;
 
-    $(function () {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const url = urlParams.get('url')
-        //console.log(url);
+$downloader = new YouTubeDownloader();
+const SWIFT = 'https://www.youtube.com/watch?v=e-ORhEE9VVg';
 
-        var oThis = $(this);
-        oThis.attr('disabled', true);
+$ret = $downloader->getDownloadLinks(self::SWIFT);
 
-        $.get('video_info.php', {url: url}, function (data) {
+echo count($ret);
 
-            oThis.attr('disabled', false);
-
-           // console.log(data);
-
-            // first link with video
-            var first = data.find(function (link) {
-                return link['format'].indexOf('video') !== -1;
-            });
-
-            console.log(first);
-
-           // echo '{"title":"' . $videoTitle . '", "format":"'.$videoFormat.'", "url":"' . $downloadURL . '"}';
-
-            var stream_url = 'stream.php?url=' + encodeURIComponent(first['url']);
-
-            console.log("{url:"+stream_url+"}");
-
-            /*var video = $("video");
-            video.attr('src', stream_url);
-            video[0].load();*/
-        });
-
-
-    });
-</script>
-
-</body>
-</html>
 
