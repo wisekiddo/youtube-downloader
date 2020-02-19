c557c46a3d5b146f651d4cda7d2e423455e51c71
mojave <ronald@wisekiddo.com>
2020-02-19 18:50:16 +0800

    asdfsdf

--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
 
 
 use YouTube\YouTubeDownloader;
+echo "Hello World!";
+
 
 $downloader = new YouTubeDownloader();
 const SWIFT = 'https://www.youtube.com/watch?v=e-ORhEE9VVg';
@@ -9,5 +20,9 @@ const SWIFT = 'https://www.youtube.com/watch?v=e-ORhEE9VVg';
 $ret = $downloader->getDownloadLinks(self::SWIFT);
 
 echo count($ret);
+?>
+
+</body>
+</html>
 
 
