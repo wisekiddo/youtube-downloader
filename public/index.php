c640e35b68efcc6227c2cb709fd6fc588abba1a2
mojave <ronald@wisekiddo.com>
2020-02-19 19:03:48 +0800

    arranged

--- public/index.php
+++ public/index.php
@@ -10,11 +10,8 @@ use YouTube\YouTubeDownloader;
 $youtubeURL = htmlspecialchars($_GET["url"]);
 echo $youtubeURL;
 
-$downloader = new YouTubeDownloader();
-const SWIFT = 'https://www.youtube.com/watch?v=e-ORhEE9VVg';
-
-$youtube = new \YouTube\YouTubeDownloader();
-$links = $youtube->getDownloadLinks(SWIFT);
+$youtube = new YouTubeDownloader();
+$links = $youtube->getDownloadLinks($youtubeURL);
 
 header('Content-Type: application/json');
 echo json_encode($links, JSON_PRETTY_PRINT);
