97391cbd92097b4a057fd9ded8a2c31de0a8d539
mojave <ronald@wisekiddo.com>
2020-02-19 20:39:44 +0800

    filtering result based on tag

--- public/index.php
+++ public/index.php
@@ -10,27 +10,37 @@ $youtubeURL = htmlspecialchars($_GET["url"]);
 $youtube = new YouTubeDownloader();
 $links = $youtube->getDownloadLinks($youtubeURL);
 
-//echo ' oead' . $links;
-
-
 echo json_encode($links, JSON_PRETTY_PRINT);
 
+$videoTitle = "video";
+$downloadURL = "";
+$videoFormat = "";
 
 for ($x = 0; $x < count($links); $x++) {
 
     $videoFormatHold = $links[$x]['format'];
+    $videoTag = $links[$x]['itag'];
 
-    if (strpos($videoFormatHold, 'mp4') !== true)
+
+    if( strcasecmp($videoTag,'59') ==0 || strcasecmp($videoTag,'78')==0 || strcasecmp($videoTag,'83')==0 || strcasecmp($videoTag,'82')==0){
+        $videoTitle = "video";
+        $downloadURL = $links[$x]['url'];
+        $videoFormat = $links[$x]['format'];
+
+    }
+
+    else if( strcasecmp($videoTag,'84') ==0 || strcasecmp($videoTag,'85')==0){
+        $videoTitle = "video";
+        $downloadURL = $links[$x]['url'];
+        $videoFormat = $links[$x]['format'];
         break;
-
-    $videoTitle   = "video";
-    $downloadURL  = $links[$x]['url'];
-    $videoFormat = $links[$x]['format'];
+    }
 
 }
 
-echo '{"title":"' . $videoTitle . '", "format":"'.$videoFormat.'", "url":"' . $downloadURL . '"}';
+$result = '{"title":"' . $videoTitle . '", "format":"' . $videoFormat . '", "url":"' . $downloadURL . '"}';
 
 
+echo json_encode($result, JSON_PRETTY_PRINT);
 
 
